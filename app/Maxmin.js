9856f97f59f511ff96a2b54a9dcf1e46468348a1
unknown <damilola.durodola@ymail.com>
2016-11-29 13:05:17 +0100

    Modified Maxmin program

--- app/Maxmin.js
+++ app/Maxmin.js
@@ -1,32 +1,22 @@
-module.exports = 
-{
-	findMinMax:function (numbers)
-	{
+module.exports = {
+	findMinMax: function (numbers) {
 		min = numbers[0];
 		max = numbers[0];
-		
-		for (var i = 0; i < numbers.length; i++) 
-		{
-			
-			if (numbers[i] < min) 
-			{
-				min = numbers[i];
-			}
 
-			else if (numbers[i] > max)
-			{
+		for (var i = 0; i < numbers.length; i++) {
+
+			if (numbers[i] < min) {
+				min = numbers[i];
+			} else if (numbers[i] > max) {
 				max = numbers[i];
 			}
 		}
-			
-			if (min == max)
-			{
-			  return[max];
-			}
-			else
-			{
-			  return [min, max]
-			}
+
+		if (min == max) {
+			return [max];
+		} else {
+			return [min, max]
+		}
 
 	}
 }
--- tests/MaxminTest.js
+++ tests/MaxminTest.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
   var chai = require('chai');
   var expect = chai.expect;
